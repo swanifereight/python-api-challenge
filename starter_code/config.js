65e5d122f314cb830c219607c2b59da8dab78236
swanifereight <72227781+swanifereight@users.noreply.github.com>
2020-12-15 14:59:18 -0500

    Delete config.js

--- starter_code/config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-	gkey = "AIzaSyBxD9FerYTD4gFLTPGXiYtNqK9cNIVnoT8",
-	openweather = "8937d0df02e42a145cf82db9770cb20a",
-	omdb = "7c07411b",
-	census = "ff73697ae7ca1b6f9b5903c8f24f8bfb11a60d81"
-
-}
